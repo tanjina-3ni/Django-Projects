ca192a2f0059e873386711432ebc97f3271a1308
Tanjina Rahman <tanjina.3ni@gmail.com>
2021-02-23 00:26:16 +0600

    initial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn template_project.wsgi --log-file
--- template_project/settings.py
+++ template_project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'hz545zkvrs53%=tb6#2b683qcjmb!2^rf)cbq9bs%5up7g1x66'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
